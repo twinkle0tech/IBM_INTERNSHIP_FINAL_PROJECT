cd3c9fe08963025cd247993de2b09a0a206676d8
Muskan Tamang <muskantamang925@gmail.com>
2026-08-28 15:04:19 +0530

    Update Login.jsx

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 
-const API_URL = "http://localhost:5000";
+const API_URL = "https://ibm-internship-final-project.onrender.com";
 
 function Login({ onLogin, goToRegister }) {
   const [email, setEmail] = useState("");
