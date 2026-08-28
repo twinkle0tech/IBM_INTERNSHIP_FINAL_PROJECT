fb7ff05853d50ec149aca02a52e9f57441c53320
Muskan Tamang <muskantamang925@gmail.com>
2026-08-28 14:59:33 +0530

    Update API_URL to production endpoint

--- frontend/src/pages/Courses.jsx
+++ frontend/src/pages/Courses.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 
-const API_URL = "http://localhost:5000";
+const API_URL = "https://ibm-internship-final-project.onrender.com";
 
 
 function Courses() {
