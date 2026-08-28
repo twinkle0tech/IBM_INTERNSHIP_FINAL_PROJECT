53e9a3ade7af90405ce2d9e5cecefc4071f1f3bf
Muskan Tamang <muskantamang925@gmail.com>
2026-08-28 15:40:33 +0530

    Update API_URL to new endpoint

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 
-const API_URL = "https://ibm-internship-final-project.onrender.com";
+const API_URL = "https://ibm-internship-final-project-2.onrender.com";
 
 function Login({ onLogin, goToRegister }) {
   const [email, setEmail] = useState("");
