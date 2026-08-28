28e67cc572349d93da94ee29049a75452a81e585
Muskan Tamang <muskantamang925@gmail.com>
2026-08-28 15:41:20 +0530

    Update API_URL to new endpoint

--- frontend/src/pages/Settings.jsx
+++ frontend/src/pages/Settings.jsx
@@ -1,6 +1,6 @@
 import { useRef, useState } from "react";
 
-const API_URL = "https://ibm-internship-final-project.onrender.com";
+const API_URL = "https://ibm-internship-final-project-2.onrender.com";
 
 function Settings({ user, onUserUpdate }) {
   const fullName = user?.name || "Dianne Russell";
